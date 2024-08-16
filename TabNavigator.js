20b5bb1fc2dc3a9781f735dbd952e24d874b8f41
Willfoss <118803774+Willfoss@users.noreply.github.com>
2024-08-16 17:04:14 +0100

    centered headers of each tab

--- TabNavigator.js
+++ TabNavigator.js
@@ -10,45 +10,45 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator initialRouteName="My Plants">
+    <Tab.Navigator screenOptions={{ headerTitleAlign: "center" }} initialRouteName="My Plants">
       <Tab.Screen
+        style={HeaderStyle.container}
         name="My Plants"
         component={MyPlants}
         options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="flower-outline" size={size} color={color} />
-          ),
+          tabBarIcon: ({ color, size }) => <Ionicons name="flower-outline" size={size} color={color} />,
         }}
       />
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" size={size} color={color} />
-          ),
+          tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />,
         }}
       />
       <Tab.Screen
         name="Search"
         component={Search}
         options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="search" size={size} color={color} />
-          ),
+          tabBarIcon: ({ color, size }) => <Ionicons name="search" size={size} color={color} />,
         }}
       />
       <Tab.Screen
         name="My Profile"
         component={User}
         options={{
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="user" size={size} color={color} />
-          ),
+          tabBarIcon: ({ color, size }) => <FontAwesome name="user" size={size} color={color} />,
         }}
       />
     </Tab.Navigator>
   );
+};
+
+const HeaderStyle = {
+  container: {
+    textalign: "center",
+    color: "red",
+  },
 };
 
 export default TabNavigator;
