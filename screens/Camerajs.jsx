578ea5e32623b5cde3f0a94508dc817d45db4478
Willfoss <118803774+Willfoss@users.noreply.github.com>
2024-08-16 15:42:53 +0100

    basic routing setup for the main pages

--- screens/Camerajs.jsx
+++ screens/Camerajs.jsx
@@ -1,44 +1,153 @@
-import { useState, useEffect, useRef } from "react";
-import {
-  Camera,
-  CameraView,
-  CameraType,
-  useCameraPermissions,
-} from "expo-camera";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import * as MediaLibrary from "expo-media-library";
+import React, { useState, useRef } from "react";
+import { Button, Image, Text, View, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
+import { useNavigation } from "@react-navigation/native";
 
-export default function Camerajs() {
-  const [hasCameraPermission, setHasCameraPermission] = useState(null);
-  const [image, setImage] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
-  const cameraRef = useRef(null);
-  useEffect(() => {
-    async () => {
-      MediaLibrary.requestPermissionsAsync();
-      const cameraStatus = await Camera.requestCameraPermissionsAsync();
-      setHasCameraPermission(cameraStatus.status === "granted");
-    };
-  }, []);
-  return (
-    <View style={styles.container}>
-      <Camera
-        style={styles.camera}
-        type={type}
-        FlashMode={flash}
-        ref={cameraRef}
+export default function CameraScreen() {
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [capturedImage, setCapturedImage] = useState(null);
+  const [isHidden, setisHidden] = useState(true);
+  const [error, setError] = useState(false);
+  const ref = useRef(null);
+  const navigation = useNavigation();
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#fff",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
       >
-        <Text>hello</Text>
-      </Camera>
-    </View>
+        <Text style={{ textAlign: "center" }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  const _takePhoto = async () => {
+    const photo = await ref.current.takePictureAsync();
+    setPreviewVisible(true);
+    setCapturedImage(photo.uri);
+  };
+
+  const __retakePicture = () => {
+    setCapturedImage(null);
+    setPreviewVisible(false);
+  };
+
+  const handlePress = () => {
+    setisHidden(false);
+  };
+
+  let PhotoPreview = null;
+
+  if (previewVisible && capturedImage) {
+    PhotoPreview = () => (
+      <View style={{ flex: 1 }}>
+        <Image source={{ uri: capturedImage }} style={{ flex: 9 }} />
+        <View
+          style={{
+            backgroundColor: "#C79E58",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "row",
+            minHeight: 60,
+          }}
+        >
+          <TouchableOpacity onPress={__retakePicture}>
+            <Text style={{ backgroundColor: "#7a4815", color: "#fff", fontSize: 20, padding: 5 }}>Retake Photo</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handlePress}>
+            <Text style={{ backgroundColor: "#7a4815", color: "#fff", fontSize: 20, padding: 5, marginLeft: 30 }}>Match Dog!</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+  if (error) {
+    setTimeout(() => {
+      navigation.replace("Main");
+    }, 2000);
+    return (
+      <View>
+        <Camera>
+          <View
+            style={{
+              flex: 1,
+              width: "100%",
+              height: "100%",
+              resizeMode: "contain",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "red",
+            }}
+          >
+            <Text style={{ backgroundColor: "white" }}>something went wrong</Text>
+          </View>
+        </Camera>
+      </View>
+    );
+  }
+  return (
+    <>
+      {isHidden ? (
+        <View style={{ flex: 1 }}>
+          {previewVisible && capturedImage ? (
+            <View
+              style={{
+                backgroundColor: "transparent",
+                flex: 1,
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              <PhotoPreview />
+            </View>
+          ) : (
+            <Camera onMountError={(err) => setError(true)} style={{ flex: 1, width: "100%" }} ref={ref}>
+              <View
+                style={{
+                  position: "absolute",
+                  bottom: 0,
+                  flexDirection: "row",
+                  flex: 1,
+                  width: "100%",
+                  padding: 20,
+                  justifyContent: "space-between",
+                }}
+              >
+                <View
+                  style={{
+                    alignSelf: "center",
+                    flex: 1,
+                    alignItems: "center",
+                  }}
+                >
+                  <TouchableOpacity
+                    onPress={_takePhoto}
+                    style={{
+                      width: 70,
+                      height: 70,
+                      bottom: 0,
+                      borderRadius: 50,
+                      backgroundColor: "#fff",
+                    }}
+                  />
+                </View>
+              </View>
+            </Camera>
+          )}
+        </View>
+      ) : (
+        <p></p>
+      )}
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-  },
-});
---
+++ screens/Search.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+import { Text } from "react-native";
+export default function User() {
+  return <Text>SEARCH</Text>;
+}
