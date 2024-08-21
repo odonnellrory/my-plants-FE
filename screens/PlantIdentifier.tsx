7ea422250169e66f7dbd65af8808e9f6b9b44687
Willfoss <118803774+Willfoss@users.noreply.github.com>
2024-08-21 11:53:33 +0100

    console logs removed

--- screens/PlantIdentifier.tsx
+++ screens/PlantIdentifier.tsx
@@ -4,7 +4,7 @@ import * as FileSystem from "expo-file-system";
 import axios from "axios";
 import IdentifiedPlantCard from "../Components/IdentifiedPlantCard";
 
-const API_KEY = process.env.REACT_APP_PLANT_ID_API_KEY;
+const API_KEY = process.env.REACT_APP_PLANT_ID_API_KEY; //within .env file
 const API_URL = process.env.REACT_APP_PLANT_ID_API_URL;
 
 interface PlantSuggestion {
