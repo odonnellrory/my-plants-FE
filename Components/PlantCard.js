7c2d6e8f2fd291e4d4e8546ecef64b75ace10351
odonnellrory <roryod.134@gmail.com>
2024-08-22 11:05:38 +0100

    push notifications working with reminder based on next_watering from plant object, currently local

--- Components/PlantCard.js
+++ Components/PlantCard.js
@@ -1,45 +1,57 @@
 import React from "react";
-import { Text, View, Image, Pressable } from "react-native";
+import { Text, View, Image, Pressable, StyleSheet } from "react-native";
 import { SimpleLineIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import PushNotification from "./PushNotification";
 
-export default function PlantCard(props) {
+export default function PlantCard({ plant }) {
   let navigation = useNavigation();
-  const { plant } = props;
 
   function handlePlantCardPress() {
     navigation.navigate("SinglePlant", { plant, name: plant.plant_name });
   }
 
   return (
-    <Pressable style={styles.container} onPress={handlePlantCardPress}>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{ uri: plant.img_url }}></Image>
-        <Text>{plant.plant_nickname}</Text>
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.text}>{plant.plant_name}</Text>
-        <View style={styles.conditionContainer}>
-          <SimpleLineIcons style={styles.icons} name="drop"></SimpleLineIcons>
-          <Text style={styles.conditionText}>Next Watering {plant.next_watering}</Text>
+    <View style={styles.container}>
+      <Pressable onPress={handlePlantCardPress}>
+        <View style={styles.cardContent}>
+          <View style={styles.imageContainer}>
+            <Image style={styles.image} source={{ uri: plant.img_url }} />
+            <Text>{plant.plant_nickname}</Text>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>{plant.plant_name}</Text>
+            <View style={styles.conditionContainer}>
+              <SimpleLineIcons style={styles.icons} name="drop" />
+              <Text style={styles.conditionText}>
+                Next Watering {plant.next_watering}
+              </Text>
+            </View>
+          </View>
         </View>
+      </Pressable>
+      <View style={styles.notificationContainer}>
+        <PushNotification plant={plant} compact={true} />
       </View>
-    </Pressable>
+    </View>
   );
 }
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
-    alignSelf: "stretch",
-    flexDirection: "row",
     backgroundColor: "#C0DEAD",
     marginBottom: 10,
     borderRadius: 4,
     padding: 10,
+    overflow: "hidden",
+  },
+  cardContent: {
+    flexDirection: "row",
   },
   image: {
     width: 100,
     height: 100,
+    borderRadius: 4,
   },
   imageContainer: {
     justifyContent: "center",
@@ -48,22 +60,28 @@ const styles = {
   textContainer: {
     flex: 1,
     marginLeft: 15,
-    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     textAlign: "center",
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: "bold",
+    marginBottom: 5,
   },
   conditionContainer: {
-    fontSize: 12,
-    justifyContent: "center",
+    flexDirection: "row",
     alignItems: "center",
-    paddingTop: 5,
-    paddingBottom: 5,
+    justifyContent: "center",
   },
   icons: {
-    fontSize: 50,
+    fontSize: 20,
     color: "#20a7db",
+    marginRight: 5,
   },
-};
+  conditionText: {
+    fontSize: 14,
+  },
+  notificationContainer: {
+    marginTop: 10,
+  },
+});
--- screens/SinglePlant.js
+++ screens/SinglePlant.js
@@ -1,48 +1,56 @@
 import { useRoute } from "@react-navigation/native";
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, ScrollView, StyleSheet } from "react-native";
 import { SimpleLineIcons, Feather } from "@expo/vector-icons";
+import PushNotification from "../Components/PushNotification";
 
 export default function SinglePlant(props) {
   const route = useRoute();
   const plant = route.params?.plant;
+
   return (
-    <View style={styles.screenContainer}>
-      <View style={styles.contentContainer}>
+    <ScrollView
+      style={styles.screenContainer}
+      contentContainerStyle={styles.contentContainer}
+    >
+      <View style={styles.plantInfoContainer}>
         <Text style={styles.nickname}>{plant.plant_nickname}</Text>
-        <Image style={styles.image} source={{ uri: plant.img_url }}></Image>
+        <Image style={styles.image} source={{ uri: plant.img_url }} />
       </View>
       <View style={styles.conditionContainer}>
         <View style={styles.sunWaterContainer}>
-          <Feather name="sun" style={styles.sunIcon}></Feather>
+          <Feather name="sun" style={styles.sunIcon} />
           <Text>This plant does best in a {plant.sunlight} environment</Text>
         </View>
         <View style={styles.sunWaterContainer}>
-          <SimpleLineIcons style={styles.waterIcon} name="drop"></SimpleLineIcons>
+          <SimpleLineIcons style={styles.waterIcon} name="drop" />
           <Text>This plant requires {plant.water} watering</Text>
         </View>
       </View>
       <Text style={styles.description}>{plant.plant_description}</Text>
-    </View>
+      <PushNotification plant={plant} />
+    </ScrollView>
   );
 }
 
-const styles = {
+const styles = StyleSheet.create({
   screenContainer: {
     flex: 1,
     backgroundColor: "#fff4c7",
-    alignItems: "center",
-    justifyContent: "flex-start",
   },
   contentContainer: {
+    alignItems: "center",
+    justifyContent: "flex-start",
+    padding: 10,
+  },
+  plantInfoContainer: {
     alignSelf: "stretch",
-    flex: 1,
     backgroundColor: "#C0DEAD",
     alignItems: "center",
     justifyContent: "flex-start",
     borderRadius: 10,
     margin: 10,
-    maxHeight: 360,
+    padding: 10,
   },
   image: {
     width: 300,
@@ -54,20 +62,22 @@ const styles = {
     padding: 10,
     borderRadius: 10,
     backgroundColor: "#ed8a53",
+    alignSelf: "stretch",
   },
   conditionContainer: {
     alignSelf: "stretch",
     flexDirection: "row",
     alignItems: "stretch",
-    justifyContent: "center ",
+    justifyContent: "center",
+    marginVertical: 10,
   },
   sunWaterContainer: {
-    flexBasis: "100%",
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
     maxWidth: 150,
-    margin: 10,
-    padding: 4,
+    margin: 5,
+    padding: 10,
     borderWidth: 2,
     borderColor: "#ED8A53",
     borderRadius: 6,
@@ -83,5 +93,6 @@ const styles = {
   nickname: {
     fontSize: 20,
     fontWeight: "bold",
+    marginBottom: 10,
   },
-};
+});
